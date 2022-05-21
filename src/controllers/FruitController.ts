cb9c4caf28f88704eb84f7bd4d4f645ab5dba7ed
jeonghoon <shb03323@naver.com>
2022-05-22 04:52:33 +0900

    [FEAT] add putWateringCount controller

--- src/controllers/FruitController.ts
+++ src/controllers/FruitController.ts
@@ -124,6 +124,36 @@ const getMyFruits = async (req: Request, res: Response) => {
     }
 };
 
+/**
+ *  @route PUT /fruit/:fruitId/water
+ *  @desc UPDATE all the fruits on tree's onTree to false
+ *  @access Public
+ */
+const miningFruits = async (req: Request, res: Response) => {
+    const { fruitId } = req.params;
+    try {
+        const data = await FruitService.putWateringCount(fruitId);
+
+        if (!data) {
+            return res
+                .status(statusCode.NOT_FOUND)
+                .send(util.fail(statusCode.NOT_FOUND, message.NOT_FOUND));
+        }
+
+        res.status(statusCode.OK).send(
+            util.success(statusCode.OK, message.PUT_FRUIT_WATERING_COUNT_SUCCESS, data)
+        );
+    } catch (error) {
+        console.log(error);
+        res.status(statusCode.INTERNAL_SERVER_ERROR).send(
+            util.fail(
+                statusCode.INTERNAL_SERVER_ERROR,
+                message.INTERNAL_SERVER_ERROR
+            )
+        );
+    }
+};
+
 export default {
     getFruits,
     findFruitById,
